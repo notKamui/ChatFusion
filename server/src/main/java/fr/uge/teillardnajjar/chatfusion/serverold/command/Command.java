790a0779d1673bd4378d6ba4f669c9430f8a7638
notKamui <teillard.jimmy@outlook.fr>
2022-05-01 23:52:22 +0200

    Deleted old server files

--- server/src/main/java/fr/uge/teillardnajjar/chatfusion/serverold/command/Command.java
+++
@@ -1,7 +0,0 @@
-package fr.uge.teillardnajjar.chatfusion.serverold.command;
-
-import fr.uge.teillardnajjar.chatfusion.serverold.logic.Server;
-
-public interface Command {
-    void execute(Server server);
-}
